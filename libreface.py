19061dd42d81ecbe639c4b3262382db2cb0bca9a
saveli <abhaltenimlos@gmail.com>
2024-03-08 14:30:24 +0100

    multiprocessing

--- libreface.py
+++ libreface.py
@@ -4,6 +4,11 @@ from PIL import Image
 import urllib.request
 from pathlib import Path
 import pandas as pd
+import os
+from tqdm.contrib.concurrent import process_map
+
+input = 'data/raw_face_aligned'
+output = 'data/raw_libreface'
 
 if not Path('au_enc.onnx').exists():
     urllib.request.urlretrieve(
@@ -32,25 +37,33 @@ au_det = ort.InferenceSession('au_det.onnx', providers=['CPUExecutionProvider'])
 au_reg = ort.InferenceSession('au_reg.onnx', providers=['CPUExecutionProvider'])
 fer = ort.InferenceSession('fer.onnx', providers=['CPUExecutionProvider'])
 
-images_preprocessed = []
-for file in Path('data/raw_face_aligned/04595').glob('*.jpg'):
-    image = np.array(Image.open(file).resize((224, 224), Image.LANCZOS))
-    image = image.astype(float)
-    image = image / 255.0
-    # supposedly RGB
-    image = image - [[[0.485, 0.456, 0.406]]]
-    image = image / [[[0.229, 0.224, 0.225]]]
-    image = image.transpose((2, 0, 1))
-    image = image.astype(np.float32)
-    images_preprocessed.append(image)
+def method(folder):
+    images_preprocessed = []
+    for file in sorted(folder.glob('*.jpg')):
+        image = np.array(Image.open(file).resize((224, 224), Image.LANCZOS))
+        image = image.astype(float)
+        image = image / 255.0
+        # supposedly RGB
+        image = image - [[[0.485, 0.456, 0.406]]]
+        image = image / [[[0.229, 0.224, 0.225]]]
+        image = image.astype(np.float32)
+        image = image.transpose((2, 0, 1))
+        images_preprocessed.append(image)
 
 images_preprocessed_encoded = [au_enc.run(['feature'], {'image': np.expand_dims(x, axis=0)}) for x in images_preprocessed]
 
-preds = [[au_det.run(['au_presence'], {'feature': np.expand_dims(np.squeeze(x), axis=0)}) for x in images_preprocessed_encoded],
-         [au_reg.run(['au_intensity'], {'feature': np.expand_dims(np.squeeze(x), axis=0)}) for x in images_preprocessed_encoded],
-         [fer.run(['FEs', 'onnx::Gemm_204'], {'image': np.expand_dims(x, axis=0)}) for x in images_preprocessed]]
+    preds = [[au_det.run(['au_presence'], {'feature': np.expand_dims(np.squeeze(x), axis=0)}) for x in images_preprocessed_encoded],
+             [au_reg.run(['au_intensity'], {'feature': np.expand_dims(np.squeeze(x), axis=0)}) for x in images_preprocessed_encoded],
+             [fer.run(['FEs', 'onnx::Gemm_204'], {'image': np.expand_dims(x, axis=0)}) for x in images_preprocessed]]
 
 preds, fer_feats = np.concatenate(list((*preds[:2], [[[y]] for x in preds[2] for y in x[0]])), axis=-1).squeeze(), np.concatenate([[y] for x in preds[2] for y in x[1]])
 df = pd.DataFrame(preds, columns=labels)
 df['fer_feats'] = [x for x in fer_feats]
 print('bla')
+    preds, fer_feats = np.concatenate(list((*preds[:2], [[[y]] for x in preds[2] for y in x[0]])), axis=-1).squeeze(), np.concatenate([[y] for x in preds[2] for y in x[1]])
+    df = pd.DataFrame(preds, columns=labels)
+    df['fer_feats'] = [x for x in fer_feats]
+    df.to_pickle(Path(output) / (folder.name + '.xz'))
+
+folders = list(Path(input).glob('*'))
+process_map(method, folders, max_workers=int(os.cpu_count()/2))
